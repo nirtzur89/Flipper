7f518a399230243b3b66f1582e1a8949d6617d32
nirtzur <nirtzur89@gmail.com>
2019-11-08 00:09:08 +0100

    boiler setup 123

---
+++ src/playground/redux-start.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+
+const flipState = {
+  words: [
+    {
+      id: 'hahahahahaha',
+      word: 'machen',
+      translation: 'doing',
+      derdie: 'das',
+      family: 'activities'
+    }
+  ],
+  filters: {
+      
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/redux-start.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
