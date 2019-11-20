a1c25df8f772774a0f7253b953f5b9b2ad07ec54
nirtzur <nirtzur89@gmail.com>
2019-11-20 23:05:32 +0100

    store creation part 1

--- src/playground/redux-start.js
+++ src/playground/redux-start.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 
-const flipState = {
+//Notes reducer
+const notesReducerDefaultState = [];
+const notesReducer = (state = notesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Filters reducer
+const filtersReducerDefaultState = {
+  family: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Store Creation
+const store = createStore(
+  combineReducers({
+    notes: notesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
+const flipperState = {
   words: [
     {
       id: 'hahahahahaha',
-      word: 'machen',
-      translation: 'doing',
+      word: 'kind',
+      translation: 'child',
       derdie: 'das',
-      family: 'activities'
+      family: 'people',
+      createdAt: 0
     }
   ],
   filters: {
-      
+    family: 'people',
+    sortBy: 'time',
+    startDate: undefined,
+    endDate: undefined
   }
 };
